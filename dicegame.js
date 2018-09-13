3bd10f5edfcf6080feceb836c2660237c30c5b89
TyVoe97 <tydud2016@gmail.com>
2018-09-13 16:26:23 -0500

    pushing Dice Game

---
+++ dicegame.js
@@ -0,0 +1,53 @@
+"use strict";
+
+  
+      
+    
+
+
+
+function rollDice1(){
+    // first pair of dice 
+  
+    let die1 = document.getElementById("die1");
+    let die2 = document.getElementById("die2");
+    let status = document.getElementById("status");
+    let d1 = Math.floor(Math.random() * 4) + 1;
+    let d2 = Math.floor(Math.random() * 4) + 1;
+    let diceTotal = d1 + d2;
+    die1.innerHTML = d1;
+    die2.innerHTML = d2;
+    status.innerHTML= "You got some numbers yay" + diceTotal+".";
+    
+}
+function rollDice2(){
+    let die3 = document.getElementById("die3");
+    let die4 = document.getElementById("die4");
+    let status = document.getElementById("status");
+    let d3 = Math.floor(Math.random() * 6) + 1;
+    let d4 = Math.floor(Math.random() * 6) + 1;
+    let diceTotal = d3 + d4;
+    die3.innerHTML = d3;
+    die4.innerHTML = d4;
+    status.innerHTML= "You got some numbers yay" + diceTotal+".";
+}
+
+
+
+    let die5 = document.getElementById("die5");
+    let die6 = document.getElementById("die6");
+    let status = document.getElementById("status");
+    
+    
+    let d5 = Math.floor(Math.random() * 8) + 1;
+    let d6 = Math.floor(Math.random() * 8) + 1;
+    let diceTotal = d1 +d2 +d3 + d4 + d5 + d6;
+    
+    
+    die5.innerHTML = d5;
+    die6.innerHTML = d6;
+    
+    
+        
+console.log(rollDice1());
+console.log(rollDice2());
